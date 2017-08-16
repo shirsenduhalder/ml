b80628aaa6d9a868202019fe2bf56d162d864c4b
Shirsendu Halder <haldershirsendu1@gmail.com>
2017-08-16 23:27:16 +0530

    best fit line prediction

--- linearregression.py
+++ linearregression.py
@@ -1,7 +1,9 @@
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt  
+from matplotlib import style
 
+style.use('fivethirtyeight')
 # xs = [1,2,3,4,5]
 # ys = [5,4,6,5,6]
 # plt.scatter(xs,ys)
@@ -10,10 +12,20 @@ import matplotlib.pyplot as plt
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
-def best_fit_slope(xs,ys):
+def best_fit_slope_and_intercept(xs,ys):
 	m = (((mean(xs)*mean(ys))- mean(xs*ys))/
 		((mean(xs))**2 - mean(xs*xs)))
-	return m 
+	b = mean(ys) - m*mean(xs)
+	return m ,b
 
-m = best_fit_slope(xs,ys)
-print(m)
+m, b = best_fit_slope_and_intercept(xs,ys)
+
+# print(m,b)
+
+regression_line = [(m*x)+b for x in xs]
+predict_x = 8
+predict_y = (m*predict_x) + b
+plt.scatter(xs,ys)
+plt.scatter(predict_x,predict_y,color='green')
+plt.plot(xs, regression_line)
+plt.show()
